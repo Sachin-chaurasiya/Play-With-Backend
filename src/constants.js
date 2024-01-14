008a4c969ef1f9994846907ab5eea00b4676799c
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-14 09:42:59 +0530

    update register user logic

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,3 @@
 export const DB_NAME = "videotube";
 export const API_ROUTE_PREFIX = "/api/v1";
+export const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,7 +1,72 @@
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { User } from "../models/user.model.js";
+import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { EMAIL_REGEX } from "../constants.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res) => {
-  return res.status(200).json({ message: "Play With Backend" });
+  // get user details from the client request
+  // validation - not empty, valid email, etc.
+  // check if user already exists: username, email
+  // check for images, check for avatar
+  // upload images to cloudinary, avatar
+  // create user object - create user in the database
+  // remove password and refresh token from user object
+  // check for user creation
+  // return response to the client
+
+  const { username, email, fullName, password } = req.body;
+
+  if (
+    [username, email, fullName, password].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "username, email, full name and password required");
+  }
+
+  // validate email
+  if (!EMAIL_REGEX.test(email)) {
+    throw new ApiError(400, "Invalid email address");
+  }
+
+  const existedUser = await User.findOne({ $or: [{ username }, { email }] });
+
+  if (existedUser) {
+    throw new ApiError(409, "User with this username or email already exists");
+  }
+
+  const avatarLocalPath = req.files?.avatar[0]?.path;
+  const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+  if (!avatarLocalPath) {
+    throw new ApiError(400, "Avatar image is required");
+  }
+  const avatar = await uploadOnCloudinary(avatarLocalPath);
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!avatar) {
+    throw new ApiError(500, "Avatar upload failed");
+  }
+
+  const user = await User.create({
+    username: username.toLowerCase(),
+    email,
+    fullName,
+    password,
+    avatar: avatar.url,
+    coverImage: coverImage?.url || "",
+  });
+
+  const createdUser = await User.findById(user._id).select(
+    "-password -refreshToken"
+  );
+
+  if (!createdUser) {
+    throw new ApiError(500, "User creation failed");
+  }
+  return res
+    .status(201)
+    .json(new ApiResponse(201, createdUser, "User registered successfully"));
 });
 
 export { registerUser };
