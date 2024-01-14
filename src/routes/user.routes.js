d9697a6e307cefaf8e7efbebd07c01216d154a8c
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-14 23:11:31 +0530

    Add getUserChannelProfile and getWatchHistory routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -9,6 +9,8 @@ import {
   updateAccountDetails,
   updateCoverImage,
   updateUserAvatar,
+  getUserChannelProfile,
+  getWatchHistory,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -37,5 +39,8 @@ router
 router
   .route("/me/cover-image")
   .patch(verifyJWT, upload.single("coverImage"), updateCoverImage);
+
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 
 export default router;
