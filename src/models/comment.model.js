cbac1c2671ada945c80644d76933188b086f4f3d
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-15 12:28:20 +0530

    Add comment and like models

---
+++ src/models/comment.model.js
@@ -0,0 +1,21 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema(
+  {
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Comment = mongoose.model("Comment", commentSchema);
