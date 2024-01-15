6a0906115f326ab09fcfe2917b73d98033842ef8
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-15 20:44:50 +0530

    Refactor tweet controller to handle error cases and improve code readability

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -1,24 +1,112 @@
-import mongoose, { isValidObjectId } from "mongoose";
 import { Tweet } from "../models/tweet.model.js";
-import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { RESPONSE_STATUS_CODE } from "../constants.js";
 
 const createTweet = asyncHandler(async (req, res) => {
-  //TODO: create tweet
+  const { content } = req.body;
+
+  if (!content) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Content is required"])
+      );
+  }
+
+  const tweet = await Tweet.create({
+    content,
+    owner: req.user?._id,
+  });
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.CREATED,
+    tweet,
+    "Tweet created successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.CREATED).json(response);
 });
 
 const getUserTweets = asyncHandler(async (req, res) => {
-  // TODO: get user tweets
+  const { userId } = req.params;
+
+  if (!userId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["User ID is required"])
+      );
+  }
+
+  const tweets = await Tweet.find({ owner: userId });
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    tweets,
+    "Tweets fetched successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const updateTweet = asyncHandler(async (req, res) => {
-  //TODO: update tweet
+  const { tweetId } = req.params;
+
+  if (!tweetId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Tweet ID is required"])
+      );
+  }
+
+  const { content } = req.body;
+
+  if (!content) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Content is required"])
+      );
+  }
+
+  const tweet = await Tweet.findByIdAndUpdate(
+    tweetId,
+    { $set: { content } },
+    { new: true }
+  );
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    tweet,
+    "Tweet updated successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const deleteTweet = asyncHandler(async (req, res) => {
-  //TODO: delete tweet
+  const { tweetId } = req.params;
+
+  if (!tweetId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Tweet ID is required"])
+      );
+  }
+
+  await Tweet.findByIdAndDelete(tweetId);
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    null,
+    "Tweet deleted successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
