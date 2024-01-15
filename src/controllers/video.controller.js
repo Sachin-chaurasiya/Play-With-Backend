78ff4828be5796dd8c724c95359ae31a08742919
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-15 21:29:11 +0530

    Add video upload functionality and update video controller

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -1,10 +1,10 @@
-import mongoose, { isValidObjectId } from "mongoose";
 import { Video } from "../models/video.model.js";
 import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import { RESPONSE_STATUS_CODE } from "../constants.js";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
@@ -13,26 +13,189 @@ const getAllVideos = asyncHandler(async (req, res) => {
 
 const publishAVideo = asyncHandler(async (req, res) => {
   const { title, description } = req.body;
-  // TODO: get video, upload to cloudinary, create video
+  const videoLocalPath = req.files?.videoFile?.[0]?.path;
+  const thumbnailLocalPath = req.files?.thumbnail?.[0]?.path;
+
+  if (!videoLocalPath || !thumbnailLocalPath) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, [
+          "Video file and thumbnail are required",
+        ])
+      );
+  }
+
+  const videoFile = await uploadOnCloudinary(videoLocalPath);
+
+  if (!videoFile) {
+    return res
+      .status(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR, [
+          "Error while uploading video file",
+        ])
+      );
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!thumbnail) {
+    return res
+      .status(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR, [
+          "Error while uploading thumbnail",
+        ])
+      );
+  }
+
+  const video = await Video.create({
+    title,
+    description,
+    videoFile: videoFile.secure_url,
+    thumbnail: thumbnail.secure_url,
+    duration: videoFile.duration,
+    owner: req.user?._id,
+  });
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    video,
+    "Video published successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: get video by id
+
+  if (!videoId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Video ID is required"])
+      );
+  }
+
+  const video = await Video.findById(videoId).populate("owner");
+
+  if (!video) {
+    return res
+      .status(RESPONSE_STATUS_CODE.NOT_FOUND)
+      .json(new ApiError(RESPONSE_STATUS_CODE.NOT_FOUND, ["Video not found"]));
+  }
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    video,
+    "Video fetched successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: update video details like title, description, thumbnail
+  const { title, description } = req.body;
+  const thumbnailLocalPath = req.file?.path;
+
+  if (!videoId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Video ID is required"])
+      );
+  }
+
+  if (!title && !description && !thumbnailLocalPath) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Nothing to update"])
+      );
+  }
+
+  const thumbnail = await uploadOnCloudinary(thumbnailLocalPath);
+
+  if (!thumbnail) {
+    return res
+      .status(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.INTERNAL_SERVER_ERROR, [
+          "Error while uploading thumbnail",
+        ])
+      );
+  }
+
+  const video = await Video.findByIdAndUpdate(
+    videoId,
+    { $set: { title, description, thumbnail: thumbnail.secure_url } },
+    { new: true }
+  );
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    video,
+    "Video updated successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const deleteVideo = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
-  //TODO: delete video
+
+  if (!videoId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Video ID is required"])
+      );
+  }
+
+  await Video.findByIdAndDelete(videoId);
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    null,
+    "Video deleted successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) {
+    return res
+      .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+      .json(
+        new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Video ID is required"])
+      );
+  }
+
+  const video = await Video.findById(videoId);
+
+  if (!video) {
+    return res
+      .status(RESPONSE_STATUS_CODE.NOT_FOUND)
+      .json(new ApiError(RESPONSE_STATUS_CODE.NOT_FOUND, ["Video not found"]));
+  }
+
+  video.isPublished = !video.isPublished;
+
+  await video.save({ validateBeforeSave: false });
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    null,
+    "Video publish status updated successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 export {
