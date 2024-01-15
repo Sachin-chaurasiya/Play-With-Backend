5cab478d99835cb3ef8af0a2ff35fdec7e0f7aae
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-15 13:53:57 +0530

    add mongooseAggregatePaginate plugin in comment schema

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const commentSchema = new Schema(
   {
@@ -17,5 +18,7 @@ const commentSchema = new Schema(
   },
   { timestamps: true }
 );
+
+commentSchema.plugin(mongooseAggregatePaginate);
 
 export const Comment = mongoose.model("Comment", commentSchema);
