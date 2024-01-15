6a563d9f2a1afdbe24c10950d8ac7313814a34af
Sachin Chaurasiya <sachinchaurasiyachotey87@gmail.com>
2024-01-15 22:28:05 +0530

    Add video search, filtering, and pagination

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -5,10 +5,72 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
 import { RESPONSE_STATUS_CODE } from "../constants.js";
+import mongoose, { isValidObjectId } from "mongoose";
 
 const getAllVideos = asyncHandler(async (req, res) => {
   const { page = 1, limit = 10, query, sortBy, sortType, userId } = req.query;
-  //TODO: get all videos based on query, sort, pagination
+
+  const pipeline = [];
+
+  if (query) {
+    pipeline.push({
+      $search: {
+        index: "search-videos",
+        text: {
+          query: query,
+          path: ["title", "description"], //search only on title, desc
+        },
+      },
+    });
+  }
+
+  if (userId) {
+    if (!isValidObjectId(userId)) {
+      return res
+        .status(RESPONSE_STATUS_CODE.BAD_REQUEST)
+        .json(
+          new ApiError(RESPONSE_STATUS_CODE.BAD_REQUEST, ["Invalid user ID"])
+        );
+    }
+
+    pipeline.push({
+      $match: {
+        owner: new mongoose.Types.ObjectId(userId),
+      },
+    });
+  }
+
+  // fetch videos only that are set isPublished as true
+  pipeline.push({ $match: { isPublished: true } });
+
+  //sortBy can be views, createdAt, duration
+  //sortType can be ascending(-1) or descending(1)
+  if (sortBy && sortType) {
+    pipeline.push({
+      $sort: {
+        [sortBy]: sortType === "asc" ? 1 : -1,
+      },
+    });
+  } else {
+    pipeline.push({ $sort: { createdAt: -1 } });
+  }
+
+  const videoAggregate = Video.aggregate(pipeline);
+
+  const options = {
+    page: parseInt(page, 10),
+    limit: parseInt(limit, 10),
+  };
+
+  const videos = await Video.aggregatePaginate(videoAggregate, options);
+
+  const response = new ApiResponse(
+    RESPONSE_STATUS_CODE.SUCCESS,
+    videos,
+    "Videos fetched successfully"
+  );
+
+  return res.status(RESPONSE_STATUS_CODE.SUCCESS).json(response);
 });
 
 const publishAVideo = asyncHandler(async (req, res) => {
